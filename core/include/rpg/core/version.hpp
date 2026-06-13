a1e56588c710cde0f8a2d843a0fc092cf5ab2e2e
Kirk Gilmore <kirklgilmore@gmail.com>
2026-06-13 15:45:23 -0700

    chore: bump version to v0.2.0

--- core/include/rpg/core/version.hpp
+++ core/include/rpg/core/version.hpp
@@ -7,7 +7,7 @@ namespace rpg::core {
 
 // rpgkit release version. Kept in lockstep with the CMake project() version,
 // enforced by version_test.
-inline constexpr std::string_view kVersion = "0.1.2";
+inline constexpr std::string_view kVersion = "0.2.0";
 
 }  // namespace rpg::core
 
